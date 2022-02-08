675f9b8026c320cce20ba12b887497eb34f3991c
Klv13 <kristian.vestrup@outlook.dk>
2022-02-08 17:30:33 +0100

    Save js

--- app.js
+++ app.js
@@ -62,5 +62,64 @@ function deleteCheck(e){
     }
 }
 
+function saveLocalTodos(todo){
+    //Check storage
+    let todos;
+    if (localStorage.getItem("todos") === null){
+            todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+ 
+function getTodos(){
+    let todos;
+    if (localStorage.getItem("todos") === null){
+        todos = [];
+    }
+    else{
+    todos = JSON.parse(localStorage.getItem("todos"));
+    }
+ 
+    todos.forEach(function(todo){
+ 
+    //Todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    //Create li
+    const newTodo = document.createElement('li')
+    newTodo.innerText = todo;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //Check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //TrashButton
+    const TrashButton = document.createElement('button');
+    TrashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    TrashButton.classList.add("trash-btn");
+    todoDiv.appendChild(TrashButton);
+    //append to list
+    todoList.appendChild(todoDiv);
+ 
+    });
+}
+function removeLocalTodos(todo){
+    //Check storage
+    let todos;
+    if (localStorage.getItem("todos") === null){
+            todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+ 
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
 
  
